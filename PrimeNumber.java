2a31c8cf95f829e51a34f4e962f7729dd690fcf5
Akash.Sharma <akashanjana08@gmail.com>
2017-12-08 11:06:14 +0100

    Modify Primenumber .java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,4 +1,23 @@
 
 public class PrimeNumber {
 
+	public static void main(String[] args) {
+		
+		
+		System.out.println(isNumberPrime(8));
+		
+		
+	}
+	
+	static boolean isNumberPrime(int number){
+		
+		for(int i=2;i<=number/2;i++)
+		{
+			if((number%i)==0){
+				return false;
+			}
+		}
+		return true;
+		
+	}
 }
